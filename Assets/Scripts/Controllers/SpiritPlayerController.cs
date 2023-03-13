6d39ae8c414452fc6d50da24c22816db0b7868f3
Vasco Correia <vasco.rosa.correia.trabalho@gmail.com>
2023-03-13 22:39:55 +0000

    Changed Input to new unity inpu package and added support for controller

--- Assets/Scripts/Controllers/SpiritPlayerController.cs
+++ Assets/Scripts/Controllers/SpiritPlayerController.cs
@@ -6,42 +6,122 @@ using UnityEngine;
 //Use arrows and numpad 0 and 1 to move spirit
 public class SpiritPlayerController : MonoBehaviour
 {
+    [SerializeField] private float _hoverHeight;
     [SerializeField] private Vector3 _velocity;
-    [SerializeField, Range(0f, 100f)] private float _maxSpeed = 10f;
-    private CharacterController _controller;
-    private Vector3 _playerInput;
+    [SerializeField] private Camera _spiritCamera;
+    [SerializeField, Range(0f, 10f)] private float maxJumpHeight = 2f;
+    [SerializeField, Range(0f, 50f)] private float _maxSpeed = 10f;
+    [SerializeField, Range(0f, 25f)] private float pushPower = 3f;
 
-    void Awake()
+    private CharacterController _controller;
+    private Vector2 _playerInput;
+    private float _ySpeed;
+
+    private void Awake()
     {
         _controller = GetComponent<CharacterController>();
     }
-
     void Update()
     {
         getPlayerMovementInput();
         calculateVelocityAndMove();
     }
 
+    private void LateUpdate()
+    {
+        applyGravity();
+    }
+
+    //gets player movement input
     private void getPlayerMovementInput()
     {
         _playerInput.x = Input.GetAxis("SpiritHorizontal");
-        _playerInput.y = Input.GetAxis("SpiritJump");
-        _playerInput.z = Input.GetAxis("SpiritVertical");
+        _playerInput.y = Input.GetAxis("SpiritVertical");
     }
 
+    //This function is reponsible for continuously applying gravity to our human.
+    private void applyGravity()
+    {
+        _ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        //if we set it to zero the _controller.isGrounded property does not work as intended so we must set it to a small negative value
+        if (isGrounded())
+        {
+            _ySpeed = 0f;
+        }
+    }
+
+    //this function calculates the velocity magnitude and direction of the current movement and after makes the character move.
+    //It is also responsible for listening to jumping input
     private void calculateVelocityAndMove()
     {
+        if (isGrounded() && Input.GetButtonDown("SpiritJump"))
+        {
+            Jump();
+        }
 
-        Vector3 movementDirection = new Vector3(_playerInput.x, _playerInput.y, _playerInput.z);
+        //get player input
+        //get cameras forward and right vectors
+        //multiply input X vector by camera right vector
+        //multiply input Z vector by camera forward vector
+        //add these two vectors
 
-        //clamping the magnitude to 1 prevents the character from moving faster diagonally.
-        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * _maxSpeed;
+        Vector3 forward = _spiritCamera.transform.forward;
+        Vector3 right = _spiritCamera.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
 
-        //after calculating the magnitude we can normalize the movement direction vector to get the direction of movement
-        movementDirection.Normalize();
+        Vector3 forwardRelativeVerticalInput = forward * _playerInput.y;
+        Vector3 rightRelativeVerticalInput = right * _playerInput.x;
 
-        _velocity = movementDirection * magnitude;
+        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
+
+        float magnitudeTest = Mathf.Clamp01(cameraRelativeMovement.magnitude) * _maxSpeed;
+
+        cameraRelativeMovement.Normalize();
+        _velocity = cameraRelativeMovement * magnitudeTest;
+        _velocity.y = _ySpeed;
+        _velocity = AdjustvelocityToSlope(_velocity);
 
         _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    //https://screenrec.com/share/62pUYiuKDW
+    public void Jump()
+    {
+        _ySpeed += Mathf.Sqrt(maxJumpHeight * -2.0f * Physics.gravity.y);
+    }
+
+    //removes bounciness when moving down slopes, keeps the direction of the movement align with the slope angle
+    private Vector3 AdjustvelocityToSlope(Vector3 velocity)
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 2f))
+        {
+            var rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            var adjustedvelocity = rotation * velocity;
+
+            //only adjust the _velocity if were moving down a slope which means out Y component of velocity must be negative, otherwise dont change velocity
+            if (adjustedvelocity.y < 0)
+            {
+                return adjustedvelocity;
+            }
+        }
+        return velocity;
+    }
+
+    private bool isGrounded()
+    {
+        Debug.DrawRay(transform.position, Vector3.down, Color.green);
+
+        if(Physics.Raycast(transform.position, Vector3.down, _hoverHeight))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/Controllers/TestInputSystem.cs
+++ Assets/Scripts/Controllers/TestInputSystem.cs
@@ -1,68 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-//The character as of now is 1.56m long in real world units or 0.78 in Unity capsule units. (normal height for 13 year old)
-public class HumanPlayerController : MonoBehaviour
+public class TestInputSystem : MonoBehaviour
 {
+    private float _ySpeed;
+    private Vector2 _playerInput;
+    private CharacterController _controller;
+    private PlayerInputAction _action;
+
+    [SerializeField] private Camera _humanCamera;
     [SerializeField] private Vector3 _velocityTest;
     [SerializeField] private bool _onGround;
-    [SerializeField] private Camera _humanCamera;
     [SerializeField, Range(0f, 10f)] private float maxJumpHeight = 2f;
     [SerializeField, Range(0f, 50f)] private float _maxSpeed = 10f;
     [SerializeField, Range(0f, 25f)] private float pushPower = 3f;
 
-    private CharacterController _controller;
-    private Vector2 _playerInput;
-    private float _ySpeed;
 
-    private void Awake()
+    void Awake()
     {
         _controller = GetComponent<CharacterController>();
+        _action = new PlayerInputAction();
+        _action.Player.Enable();
+        _action.Player.Jump.performed += Jump;
     }
+
     void Update()
     {
-        getPlayerMovementInput();
+        //_onGround = _controller.isGrounded; // inspector debuggging      
+        calculateVelocity();
         applyGravity();
-        calculateVelocityAndMove();
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    public void Jump(InputAction.CallbackContext context)
     {
-        MovePushableObject(hit.collider.attachedRigidbody, hit);
+        if (_controller.isGrounded && context.performed)
+            _ySpeed += Mathf.Sqrt(maxJumpHeight * -2.0f * Physics.gravity.y);
     }
 
-    //gets player movement input
-    private void getPlayerMovementInput()
-    {
-        _playerInput.x = Input.GetAxis("HumanHorizontal");
-        _playerInput.y = Input.GetAxis("HumanVertical");
-    }
-
-    //This function is reponsible for continuously applying gravity to our human.
     private void applyGravity()
     {
-        _onGround = _controller.isGrounded;
         _ySpeed += Physics.gravity.y * Time.deltaTime;
 
         //if we set it to zero the _controller.isGrounded property does not work as intended so we must set it to a small negative value
-        if (_onGround)
+        if (_controller.isGrounded)
         {
             _ySpeed = -0.01f;
         }
     }
-
-    //this function calculates the velocity magnitude and direction of the current movement and after makes the character move.
-    //It is also responsible for listening to jumping input
-    private void calculateVelocityAndMove()
+    public void calculateVelocity()
     {
-        if (_onGround && Input.GetButtonDown("HumanJump"))
-        {
-            Jump();
-        }
         //get player input
         //get cameras forward and right vectors
         //multiply input X vector by camera right vector
         //multiply input Z vector by camera forward vector
         //add these two vectors
+
+        _playerInput = _action.Player.Movement.ReadValue<Vector2>();
 
         Vector3 forward = _humanCamera.transform.forward;
         Vector3 right = _humanCamera.transform.right;
@@ -86,13 +79,6 @@ public class HumanPlayerController : MonoBehaviour
         _controller.Move(_velocityTest * Time.deltaTime);
     }
 
-    //https://screenrec.com/share/62pUYiuKDW
-    public void Jump()
-    {
-        _ySpeed += Mathf.Sqrt(maxJumpHeight * -2.0f * Physics.gravity.y);
-    }
-
-    //removes bounciness when moving down slopes, keeps the direction of the movement align with the slope angle
     private Vector3 AdjustvelocityToSlope(Vector3 velocity)
     {
         Debug.DrawRay(transform.position, Vector3.down);
@@ -112,10 +98,6 @@ public class HumanPlayerController : MonoBehaviour
         }
         return velocity;
     }
-
-    //OnControllerColliderHit is called when the controller hits a collider while performing a Move.
-    //We will use it to push any object with a rigidbody attached.
-    //Later if we dont want to push all rigidbodies we can add a tag to the ones we want to move and check it before we apply the logic
 
     private void MovePushableObject(Rigidbody body, ControllerColliderHit hit)
     {
