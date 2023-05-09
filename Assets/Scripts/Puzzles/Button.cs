760bfd92990c5489ffd2cca321441df7711883fd
Vasco Correia <vasco.rosa.correia.trabalho@gmail.com>
2023-05-09 20:51:43 +0100

    Spirit Button animation

--- Assets/Scripts/Puzzles/Button.cs
+++ Assets/Scripts/Puzzles/Button.cs
@@ -5,6 +5,13 @@ public class Button : MonoBehaviour, IInteractable
     [field: SerializeField] public Barrier associatedBarrier { get; private set; }
     public bool isInteracted { get; set; } = false;
 
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     public void Interacted(GameObject player)
     {
         isInteracted = true;
@@ -16,7 +23,7 @@ public class Button : MonoBehaviour, IInteractable
             player.GetComponent<RoryAnimatorManager>().PushingButtonStartHandler();
 
         }
-
+        _animator.SetTrigger("in");
         //SPIRIT ANIMATION??
         associatedBarrier.OpenBarrier();
     }
@@ -29,6 +36,7 @@ public class Button : MonoBehaviour, IInteractable
             //PlayAnimation
             player.GetComponent<RoryAnimatorManager>().StopPushingButtonAnimation();
             player.GetComponent<RoryAnimatorManager>().PushingButtonEndHandler();
+            _animator.SetTrigger("out");
             associatedBarrier.CloseBarrier();
         }
     }
